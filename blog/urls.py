254d64b3b752c087600b46ce1c5f8a9bf0d5b597
jaywomack <womackjw@hotmail.com>
2021-04-23 21:09:27 -0700

    Added a Post model and rendered the posts to the home screen. Created a test user and am getting data from the DB

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,3 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about')
 ]
-
-for i in range(10):
-    i=0
-    if i <= 10:
-        print(f'The number {i} is less than or equal to 10')
-    i= i +1
